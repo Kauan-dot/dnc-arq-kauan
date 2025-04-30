fc8c18355f761a29c376dad6b99c025c8c5e8b36
Kauan-dot <kauanaffonso115@gmail.com>
2025-04-30 14:32:07 -0300

    First commit

---
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import './LoadingSpinner.css'
+import LoadingSpinnerGif from '../../assets/loading-spinner.gif'
+
+function LoadingSpinner() {
+    return (
+        <div className='loading-overlay-container d-flex al-center jc-center'>
+            <img src={LoadingSpinnerGif} alt="Loading" height="80px"/>
+        </div>
+    )
+}
+
+export default LoadingSpinner
